d8fc23dcfd5f3e40d07b9503ac9353f4e236def1
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-09-21 09:58:50 -0600

    Updated for api deployment

--- functions/deploy/main.py
+++ functions/deploy/main.py
@@ -19,50 +19,38 @@ def deploy_to_door43(job):
     # where the files should be written after being merged into the template - /tmp/obs
     output_directory = os.path.join(tempfile.gettempdir(), 'obs')
     # the url of the obs template
-    obs_template_url = 'https://dev.door43.org/templates/obs.html'
+    obs_template_url = 'https://s3-us-west-2.amazonaws.com/test-door43.org/templates/obs-rich.html'
+
+    user, repo, commit = job['identifier'].split('/') # identifier = user/repo/commit
 
     # merge the source files with the template
-    with OBSD`oor43(source_location_url, output_directory, obs_template_url, False) as merger:
+    with OBSDoor43(source_location_url, output_directory, obs_template_url, False) as merger:
         merger.run()
 
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(job['door43_bucket'])
-    s3_project_key = 'u/{0}'.format(job['identifier'])  # identifier = <user>/<repo>/<commit>
+    s3_commit_key = 'u/{0}/{1}/{2}'.format(user, repo, commit)
+    s3_project_key = 'u/{0}/{1}'.format(user, repo)
 
     mime = MimeTypes()
     for root, dirs, files in os.walk(output_directory):
         for f in sorted(files):
             path = os.path.join(root, f)
-            key = s3_project_key + path.replace(output_directory, '')
+            key = s3_commit_key + path.replace(output_directory, '')
             mime_type = mime.guess_type(path)[0]
             if not mime_type:
                 mime_type = "text/{0}".format(os.path.splitext(path)[1])
             print('Uploading {0} to {1}, mime_type: {2}'.format(f, key, mime_type))
             bucket.upload_file(path, key, ExtraArgs={'ContentType': mime_type})
 
-    s3_resource.Object(job['door43_bucket'], 'build_log.json').copy_from(CopySource='{0}/u/{1}/build_log.json'.format(job['cdn_bucket'], job['identifier']))
-    return True
+    try:
+        s3_resource.Object(job['door43_bucket'], '{0}/build_log.json'.format(s3_commit_key)).copy_from(CopySource='{0}/{1}/build_log.json'.format(job['cdn_bucket'], s3_commit_key))
+        s3_resource.Object(job['door43_bucket'], '{0}/project.json'.format(s3_project_key)).copy_from(CopySource='{0}/{1}/project.json'.format(job['cdn_bucket'], s3_project_key))
+        s3_resource.Object(job['door43_bucket'], '{0}/manifest.json'.format(s3_commit_key)).copy_from(CopySource='{0}/{1}/manifest.json'.format(job['cdn_bucket'], s3_commit_key))
+        s3_resource.Object(job['door43_bucket'], '{0}/manifest.json'.format(s3_project_key)).copy_from(CopySource='{0}/{1}/manifest.json'.format(job['cdn_bucket'], s3_commit_key))
+    except Exception:
+        pass
 
-def download_dir(client, resource, dist, local='/tmp', bucket='your_bucket'):
-    paginator = client.get_paginator('list_objects')
-    for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
-        if result.get('CommonPrefixes') is not None:
-            for subdir in result.get('CommonPrefixes'):
-                download_dir(client, resource, subdir.get('Prefix'), local, bucket)
-        if result.get('Contents') is not None:
-            for file in result.get('Contents'):
-                if not os.path.exists(os.path.dirname(local + os.sep + file.get('Key'))):
-                    os.makedirs(os.path.dirname(local + os.sep + file.get('Key')))
-                resource.meta.client.download_file(bucket, file.get('Key'), local + os.sep + file.get('Key'))
-
-def deploy_project(from_bucket, to_bucket, key):
-    client = boto3.client('s3')
-    resource = boto3.resource('s3')
-
-    tmpdir = tempfile.mkdtemp(prefix=from_bucket)
-    download_dir(client, resource, key, tmpdir, from_bucket)
-
-    
     return True
 
 
@@ -72,25 +60,27 @@ def redeploy_all_projects(from_bucket, to_bucket):
 
 def handle(event, context):
     success = False
+
     if 'Records' in event:
         for record in event['Records']:
             # See if it is a notification from an S3 bucket
-            if "s3" in record:
+            if 's3' in record:
                 bucket = record['s3']['bucket']['name']
                 key = record['s3']['object']['key']
-
-                cdn_bucket = 'cdn.door43.org'
-                door43_bucket = 'door43.org'
-                if bucket.startswith('test-'):
-                    cdn_bucket = 'test-'+cdn_bucket
-                    door43_bucket = 'test-'+door43_bucket
-
                 # if the key ends with a .html, it was a change in template, and so all projects need to be redeployed
                 if bucket == door43_bucket and key.endswith('.html'):
-                    success = redeploy_all_projects(cdn_bucket, door43_bucket)
-                # else if it ends with build_log.json it was a change in the CDN and thus only that project needs to be generated
-                elif bucket.endswith('cdn.door43.org') and key.endswith("build_log.json"):
-                    sucess = deploy_project(cdn_bucket, door43_bucket, os.path.dirname(key))
-        return {
-            'success': success,
-        }
+                    cdn_bucket = 'cdn.door43.org'
+                    door43_bucket = 'door43.org'
+                    if bucket.startswith('test-'):
+                        cdn_bucket = 'test-'+cdn_bucket
+                        door43_bucket = 'test-'+door43_bucket
+                        success = redeploy_all_projects(cdn_bucket, door43_bucket)
+    elif 'data' in event:
+        data = event['data']
+        if 'vars' in event and isinstance(event['vars'], dict):
+            data.update(event['vars'])
+        success = deploy_to_door43(event['data'])
+
+    return {
+        'success': success
+    }
